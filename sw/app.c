03bc5763419f704fa2074e193f51c60294758758
Yann Rosema <yann.rosema@hotmail.com>
2018-12-14 16:19:58 +0100

    Memory refactor of app and ip

--- sw/app.c
+++ sw/app.c
@@ -13,13 +13,10 @@
 
 #define halt()            DBGC_HALT = 1
 
-uint32_t *BIASRAM = (uint32_t *)SYS_BIAS_RAM_BASE;
 uint32_t *INPUTRAM = (uint32_t *)SYS_INPUT_RAM_BASE;
 uint32_t *OUTPUTRAM = (uint32_t *)SYS_OUTPUT_RAM_BASE;
 
 int Iflag  = 1;
-
-char str[5] = "dick";
 
 int main ()
 {
@@ -29,33 +26,27 @@ int main ()
     print_str("Software begins\n");
     uint32_t i = 0, j = 0;
 
-    for (i = 0; i < SYS_INPUT_RAM_SIZE/1000; i++){
-        // print_str("input = "); print_int(INPUTRAM[i]); print_str("\n");
-        SYS_MEM32((SYS_AXI_BASE + BIAS_OFFSET)) = INPUTRAM[i];
+    for (i = 0; i < 16; i++){
+        print_str("input = "); print_int(INPUTRAM[i]); print_str("\n");
+        SYS_MEM32((SYS_AXI_BASE + INPUT_OFFSET)) = INPUTRAM[i];
 
-        SYS_MEM32((SYS_AXI_BASE ) ) = 0x80; // run_cnn
-        // print_str("Start DISTORT \n");
+        SYS_MEM32((SYS_AXI_BASE ) ) = 0x80;
+        print_str("Start DISTORT \n");
 
         while (Iflag);
 
-        SYS_MEM32((SYS_AXI_BASE ) ) = 0x00; // stop _cnn
-        SYS_MEM32((SYS_AXI_BASE + 4) ) = 0x00; // stop _cnn
-        // print_str("DISTORT Done\n");
+        SYS_MEM32((SYS_AXI_BASE ) ) = 0x00;
+        SYS_MEM32((SYS_AXI_BASE + 4) ) = 0x00;
+        print_str("DISTORT Done\n");
 
-        j = SYS_MEM32((SYS_AXI_BASE + OUTPUT_TENSOR_OFFSET ));
-        // print_str("output_tensor  = "); print_int(j); print_str("\n");
+        print_hex_uint((SYS_AXI_BASE + OUTPUT_OFFSET - SYS_AXI_BASE - OUTPUT_OFFSET)>>2);
+        print_str("\n");
+        j = SYS_MEM32((SYS_AXI_BASE + OUTPUT_OFFSET));
+        print_str("output  = "); print_int(j); print_str("\n");
         OUTPUTRAM[i] = j;
     }
-	
-	// for (i = 0; i < 4; i++){
-    //     j = SYS_MEM32((SYS_AXI_BASE + BIAS_OFFSET + (4*i) ));
-    //     // BIASRAM[i] = j;
-    //     print_str("char = "); print_char((char)j); print_str("\n");
-    // }
 
-
-    print_str("***********\nEVERYTHING IS DONE\n***********\n ");
-
+    print_str("***********\nEVERYTHING IS DONE\n***********\n");
 
     halt ();
 
@@ -67,6 +58,6 @@ void handle_interrupt(void)
     disableIRQ();
     SYS_MEM32((SYS_AXI_BASE ) +0) = 0x00; // stop _cnn
     SYS_MEM32((SYS_AXI_BASE ) +4) = 0x00; // stop _cnn
-    print_str("***********\nIRQ received\n***********\n ");
+    print_str("***********\nIRQ received\n***********\n");
     Iflag  = 0;
 }
